e7b71c88602991e6d3f699e983031703a6a33f9c
juanma-ge <jgomest545@g.educaand.es>
2025-03-06 16:19:43 +0100

    Push menu e inicio

---
+++ Juego.py
@@ -0,0 +1,53 @@
+def mostrar_menu():
+    while True:
+        print("\n---- MENÚ ----")
+        print("1. Iniciar modo carrera")
+        print("2. Salir")
+        
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            iniciar_modo_carrera()
+
+        elif opcion == "2":
+            print("Saliendo del juego...")
+            break
+
+        else:
+            print("Opción no válida, por favor elige una de las opciones.")
+
+
+def mostrar_equipos():
+    equipos = {
+        "Premier League": ["Manchester United", "Manchester City", "Arsenal", "Chealsea", "Tottenham", "Leicester City", "Brentford", "Nottingham Forest", "Newcastle",
+         "Bournemouth", "Aston Villa", "Fullham", "Brighton", "Crystal Palace", "West Ham", "Wolves", "Ipswich Town", "Southampton"],
+
+        "La Liga": ["Real Madrid", "Barcelona", "Atletico de Madrid", "Athletic Club", "Real Sociedad", "Betis", "Sevilla", "Villarreal", "Espanyol", "Rayo Vallecano", "Girona", "Osasuna", "Mallorca",
+        "Las Palmas", "Getafe", "Celta", "Leganés", "Valencia", "Valladolid", "Alavés"],
+
+        "Liga Hypermotyon": ["Cádiz", "Cartagena", "Granada", "Almeria", "Burgos", "Racing de Santander", "Huesca", "Elche", "Mirandés", "Real Oviedo", "Real Sporting", "Levante", "Real Zaragoza", "RC Deportivo",
+         "Albacete", "Eibar", "Cordoba", "Malaga", "Castellon", "Eldense", "Racing Ferrol", "Tenerife"],
+
+        "Bundesliga": ["RB Leipzig", "Bayer Leverkusen", "FC Bayern Munchen", "Eintracht Frankfurt", "SC Freiburg", "Mainz 05", " Stuttgart", "Wolfsburg", "Borussia Monchegladblach",
+          "Borussia Dortmund", "FC Augsburg", "Werder Bremen", "Union Berlin", "Hoffenheim", "FC ST Pauli", "Heindenheim", "VfL Bochum", "Holstein Kiel"],
+
+        "Ligue One": ["Paris Saint Germain", "Olympique Marseille", "Nice", "Lille", "Monaco", "Olympique Lyon", "Strasbourg", "Lens", "Stade Brestois", "Toulouse", "Auxerre", "Angers SCO", "Stade Rennais", "Nantes", "Stade de Reims",
+         "Saint-Étienne", "Montpellier", "Le Havre"],
+
+        "Serie A": ["Inter Milan", "AC Milan", "Napoli", "Atalanta", "Juventus", "Lazio", "Fiorentina", "Bologna", "Roma", "Udinese", "Torino", "Genoa", "Como", "Hellas Verona", "Cagliari", "Lecce", "Parma",
+         "Empoli", "Venezia", "AC Monza"]
+    }
+
+    return equipos
+
+def seleccionar_posicion():
+    
+
+def seleccionar_equipos():
+
+
+def main():
+
+
+def main():if __name__ == "__main__":
+    main()
